5e053643b06824a5e01e5c15ac50b0e874af69cc
Cyber_Infernox <sayonkar@gmail.com>
2023-10-28 23:32:09 +0530

    Footer & responsiveness

--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,10 +1,51 @@
 // Style
+import Image from "next/image";
 import styles from "./footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <h1 className={styles.logoText}>jojoBlog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
+          necessitatibus mollitia asperiores numquam a corrupti molestiae odio
+          aliquid unde temporibus error ut illum obcaecati, similique incidunt
+          ducimus, adipisci natus! Autem?
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Home</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
     </div>
   );
 };
--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>jojoblog</div>
+      <div className={styles.logo}>jojoBlog</div>
       <div className={styles.links}>
         <ThemeToggle />
         <Link className={styles.link} href="/">
